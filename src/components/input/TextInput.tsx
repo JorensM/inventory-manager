cfd120cc374b3e2a21e9b1e6f9d853282b9fcca6
JorensM <jorensmerenjanu@gmail.com>
2024-01-30 11:21:42 +0200

    feat(components): pass required prop to input element

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -11,7 +11,13 @@ export default function TextInput( { label, required, ...props }: TextInputProps
             label={label}
             required={required}
         >
-            <input type='text' {...props}></input>
+            <input 
+                type='text' 
+                {...props}
+                required={required}
+            >
+
+            </input>
         </InputBase>
     )
 }
