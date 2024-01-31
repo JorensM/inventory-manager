8d62268bb217f12376e200653765502c7124bcd4
JorensM <jorensmerenjanu@gmail.com>
2024-01-31 08:57:44 +0200

    remove env constant validation

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,27 +1,3 @@
-
-const ENV_VAR_NAMES = [
-    "NEXT_PUBLIC_SUPABASE_ANON_KEY",
-    "NEXT_PUBLIC_SUPABASE_URL",
-    "SUPABASE_ADMIN_KEY"
-];
-
-const validateEnvVars = () => {
-
-    const MISSING_VARS = [];
-
-    for(const VAR_NAME of ENV_VAR_NAMES) {
-        if(!process.env[VAR_NAME]) {
-            MISSING_VARS.push(ENV_VAR_NAMES);
-        }
-    }
-
-    if (MISSING_VARS.length) {
-        throw new Error("Missing the following environment variables: " + MISSING_VARS);
-    }
-};
-
-validateEnvVars();
-
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 export const SUPABASE_ADMIN_KEY = process.env.SUPABASE_ADMIN_KEY!;
