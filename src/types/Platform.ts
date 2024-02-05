6e49d9dee0c2f1e3fec6ffcf353b54e875d8899e
JorensM <jorensmerenjanu@gmail.com>
2024-02-05 11:32:42 +0200

    fix(types): change Platforms type to be a defined type instead of a Record
    It's hard to make a record with Generic so better to define each platform to be of its specific class's type

--- src/types/Platform.ts
+++ src/types/Platform.ts
@@ -1,6 +1,10 @@
 // Classes
 import PlatformManager from '@/classes/PlatformManager/PlatformManager'
+import ReverbManager from '@/classes/PlatformManager/ReverbManager'
 
 export type PlatformID = 'reverb' | 'ebay'
 
-export type Platforms = Record<PlatformID, PlatformManager>
+export type Platforms = {
+    reverb: ReverbManager
+    ebay: ReverbManager
+}
