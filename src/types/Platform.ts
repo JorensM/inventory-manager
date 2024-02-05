649cac7ca84885fdaa670ad2034e1701a08d8390
JorensM <jorensmerenjanu@gmail.com>
2024-02-05 11:49:14 +0200

    refactor(types): remove unused import

--- src/types/Platform.ts
+++ src/types/Platform.ts
@@ -1,5 +1,4 @@
 // Classes
-import PlatformManager from '@/classes/PlatformManager/PlatformManager'
 import ReverbManager from '@/classes/PlatformManager/ReverbManager'
 
 export type PlatformID = 'reverb' | 'ebay'
