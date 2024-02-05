5c5e757c753a8e59978e00c6418bdf647a254cae
JorensM <jorensmerenjanu@gmail.com>
2024-02-05 12:00:07 +0200

    refactor(components): add comments
    Comments make the code easier to understand

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,7 +1,12 @@
+// Core
 import { useNavigate } from 'react-router-dom';
 
+/**
+ * Back button, navigates to the previous page in history
+ */
 export default function BackButton(){
 
+    // Hooks
     const navigate = useNavigate();
 
     return (
--- src/components/input/APIKeyField.tsx
+++ src/components/input/APIKeyField.tsx
@@ -1,10 +1,17 @@
+// Components
 import TextInput, { TextInputProps } from '@/components/input/TextInput';
 import StatusIndicator from '@/components/misc/StatusIndicator';
 
+// Types
+import { Status } from '@/types/Status';
+
 type APIKeyFieldProps = TextInputProps & {
-    status: 'loading' | 'success' | 'fail' | null;
+    status: Status | null;
 };
 
+/**
+ * API Key Field. Allows you to input an API Key and also shows the ping status.
+ */
 export default function APIKeyField({ status, ...props }: APIKeyFieldProps) {
     return (
         <TextInput
