6eb3e20007493344574f7a562635d975a08a887a
JorensM <jorensmerenjanu@gmail.com>
2024-03-04 14:39:03 +0200

    fix: add team_id to CategoryCreate type because it is required to create a category

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -7,7 +7,7 @@ export type Category = {
     team_id: number,
 } & Partial<Record<PlatformID, string>>
 
-export type CategoryCreate = Omit<Category, 'id' | 'team_id'>;
+export type CategoryCreate = Omit<Category, 'id'>;
 
 export type CategoryUpdate = Partial<Category> & {
     id: number
