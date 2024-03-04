27b9e082aa135f584cabfb609f66cb799484f4f2
JorensM <jorensmerenjanu@gmail.com>
2024-03-04 15:09:11 +0200

    refactor: add comments

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,7 @@
 
+/**
+ * Env variables that should be set
+ */
 const ENV_VAR_NAMES = [
     "VITE_SUPABASE_ANON_KEY",
     "VITE_SUPABASE_URL",
@@ -6,6 +9,9 @@ const ENV_VAR_NAMES = [
     "VITE_APP_URL"
 ]
 
+/**
+ * Validates that ENV vars are set
+ */
 const validateEnvVars = () => {
     const missing_vars = []
     for(const ENV_VAR_NAME of ENV_VAR_NAMES) {
@@ -17,8 +23,9 @@ const validateEnvVars = () => {
         throw new Error('Missing the following ENV variables: ' + missing_vars);
     }
 }
-
 validateEnvVars();
+
+// Export env variables
 
 export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
