b5bf13fb2cad756bb1667927052eb584e1f16899
JorensM <jorensmerenjanu@gmail.com>
2024-03-04 14:48:44 +0200

    refactor: add comments and remove unplanned TODO

--- src/components/input/InputBase.tsx
+++ src/components/input/InputBase.tsx
@@ -1,33 +1,32 @@
 import { PropsWithChildren } from 'react';
 
 export type InputBaseProps = {
+    /**
+     * Label of field
+     */
     label: string
+    /**
+     * Whether it is required. Will show a 'required' label if true
+     */
     required?: boolean,
+    /**
+     * Direction of field contents
+     */
     contentsDirection?: 'row' | 'column'
+    /**
+     * Alignment of field contents
+     */
     contentsAlign?: 'center' | 'flex-start'
 };
 
+/**
+ * Base input component with common styling
+ */
 export default function InputBase( { label, required = false, contentsDirection = 'row', contentsAlign = 'center', children }: PropsWithChildren<InputBaseProps>) {
-
-    // #TODO: Make it so that pressing enter moves focus to the next field instead
-    // of submitting the form. More info: https://stackoverflow.com/a/70113306/1673694
-    // const containerRef = useRef<HTMLDivElement>(null);
-    // const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-    //     if (event.code == 'Enter') {
-    //         const form = containerRef.current!.closest('form');
-
-    //         if(!form) throw new Error('Input field is not in a form');
-
-
-
-    //     }
-    // }
     
     return (
         <div 
             className='input-container'
-            // onKeyDown={handleKeyDown}
-            // ref={containerRef}
         >
             <label>{label}{required ? <span className='required'>(Required)</span> : null}</label>
             <div 
