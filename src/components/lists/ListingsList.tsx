eb81ef25672099cc4b0f60e97b470649f12c9476
JorensM <jorensmerenjanu@gmail.com>
2024-03-04 15:04:18 +0200

    refactor: add comments and tidy up imports

--- src/components/lists/ListingsList.tsx
+++ src/components/lists/ListingsList.tsx
@@ -1,19 +1,35 @@
+// Core
+import { useNavigate } from 'react-router-dom';
+
 // Types
 import { Listing } from '@/types/Listing';
 
+// Constants
+import routes from '@/constants/routes';
+
 // Components
 import ListingSmall from './ListingSmall';
-import routes from '@/constants/routes';
-import { useNavigate } from 'react-router-dom';
 
 type ListingsListProps = {
     listings: Listing[]
 }
 
+/**
+ * Component to display a list of listings
+ * @param param0 
+ * @returns 
+ */
 export default function ListingsList( { listings }: ListingsListProps) {
 
+    //-- Hooks --//
     const navigate = useNavigate()
 
+    //-- Handlers --//
+
+    /**
+     * On listing item click. Navigates to appropriate listings.
+     * @param listing_id ID of listing that was clicked on
+     */
     const handleListingClick = (listing_id: number) => {
         navigate(routes.listing(listing_id))
     };
