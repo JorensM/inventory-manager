9f3b2a9e826f640d11d690e32e36397f1b72fbb2
JorensM <jorensmerenjanu@gmail.com>
2024-03-04 15:08:25 +0200

    refactor: add missing property and add comment

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,7 +1,9 @@
+// Types
 import { Settings } from '@/types/Settings';
 
 export const DEFAULT_SETTINGS: Settings = {
     reverb_key: "",
     ebay_key: "",
-    reverb_mode: 'sandbox'
+    reverb_mode: 'sandbox',
+    ebay_refresh_token: null
 }
